3015879793386f26f93cc474c87912ddc32c7c13
Carl Syner <casyner@microsoft.com>
2020-06-25 17:41:43 +0100

    update

--- on-prem.tf
+++ on-prem.tf
@@ -58,7 +58,7 @@ resource "azurerm_public_ip" "onprem-mgmt-pip" {
     name                 = "onprem-mgmt-pip"
     location            = var.location
     resource_group_name = azurerm_resource_group.private-link-microhack-onprem-rg.name
-    allocation_method   = "Dynamic"
+    allocation_method   = "static"
 
     tags = {
         environment = "onprem"
