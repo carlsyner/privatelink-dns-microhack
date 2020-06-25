ff119624aebf1dc504e205378df81f98645e4dd3
Carl Syner <casyner@microsoft.com>
2020-06-25 16:49:06 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
 output "public_ip_address" {
   description = "The actual ip address allocated for the resource."
-  value       = azurerm_public_ip.onprem-mgmt-pip.id
+  value       = azurerm_public_ip.onprem-mgmt-pip.public_ip_address
 }
